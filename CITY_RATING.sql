0e4b20d0fea081f9934ff5709fb172b7ee0c8082
AHSANRIAZ29 <105630066+AHSANRIAZ29@users.noreply.github.com>
2023-07-17 21:44:19 +0400

    Add files via upload

---
+++ CITY_RATING.sql
@@ -0,0 +1,7 @@
+INSERT INTO `CITY_RATING`(`rank_val`, `City Name`, `Customer type`, `avg_rating`) VALUES (1, 'Abu Dhabi', 'Normal', 7.06);
+INSERT INTO `CITY_RATING`(`rank_val`, `City Name`, `Customer type`, `avg_rating`) VALUES (2, 'Dubai', 'Normal', 7.05);
+INSERT INTO `CITY_RATING`(`rank_val`, `City Name`, `Customer type`, `avg_rating`) VALUES (3, 'Abu Dhabi', 'Member', 6.93);
+INSERT INTO `CITY_RATING`(`rank_val`, `City Name`, `Customer type`, `avg_rating`) VALUES (4, 'Dubai', 'Member', 6.82);
+INSERT INTO `CITY_RATING`(`rank_val`, `City Name`, `Customer type`, `avg_rating`) VALUES (5, 'Sharjah', 'Normal', 6.73);
+INSERT INTO `CITY_RATING`(`rank_val`, `City Name`, `Customer type`, `avg_rating`) VALUES (6, 'Sharjah', 'Member', 6.7);
+INSERT INTO `CITY_RATING`(`rank_val`, `City Name`, `Customer type`, `avg_rating`) VALUES (7, 'Dubai', 'NULL', 4.4);
